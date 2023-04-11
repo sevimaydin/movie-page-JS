1e6f87ad79b5c4fb0b452a89476d62eae71e39ac
sevimaydin <sevima0929@gmail.com>
2023-04-12 01:22:06 +0300

    düzenlendi

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=15005c48f2625def09dacd3d0fc01762&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=15005c48f2625def09dacd3d0fc01762&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const Search_Url = 'https://api.themoviedb.org/3/search/movie?api_key=15005c48f2625def09dacd3d0fc01762&query="';
-
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=15005c48f2625def09dacd3d0fc01762&query="';
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
+const movieContainer = document.querySelector('.movie-container')
 
+// Get initial movies
 getMovies(API_URL)
 
 async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
   showMovies(data.results)
 }
 
@@ -41,23 +41,30 @@ function showMovies(movies) {
     movieEl.classList.add('movie')
 
     movieEl.innerHTML = `
-    
-    <img src="${IMG_PATH + poster_path}" alt="${title}"
-  />
-  <div class="movie-info">
-    <h3>${title}</h3>
-    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-  </div>
-  <div class="overview">
-    <h3> ${title}  <small> Overview </small> </h3>
-    <p>
-     ${overview}
-    </p>
-  </div>
+      <img src="${IMG_PATH + poster_path}" alt="${title}" />
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>${title} <small>Overview</small></h3>
+        <p>${overview}</p>
+      </div>
     `
 
     main.appendChild(movieEl)
   })
+
+  // Create a link to go back to the home page
+  const homeLink = document.createElement('a')
+  homeLink.innerHTML = 'MovieDB'
+  homeLink.classList.add('home-link')
+  homeLink.href = '#'
+  homeLink.addEventListener('click', () => {
+    main.innerHTML = ''
+    getMovies(API_URL)
+  })
+  movieContainer.insertBefore(homeLink, main)
 }
 
 function getClassByRate(vote) {
